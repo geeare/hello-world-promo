af9d191d90f525fbb97c107fae693fec3a8b052c
GeeAre <geeare.music@gmail.com>
2023-09-24 09:03:36 -0600

    initial commit

---
+++ ascii-cover-preview.c
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+        /*
+                      GRHELLOWORLD   HELLOWORLDGR   GHELLOWORLDH  HELLOWOR
+                   GRGR:#helloworld##,helloworl;R   G;grgrgrgrgg.;grgrgr'R
+                 GR;;##hejvärlden##;'.hejvärlde;R   G;grgrgrgrgrgrgrgrgr,R
+                GR,;bonjourlemondebonjourmonde#;R   G;grgrgrgrgrgrgrgrgr,R
+               GR;;hàloashaoghailhàkoashaoghail;R   G;grgrgrgrgrgrgrgr#,'R
+               G,ciaomondo;GRHELLOW;:ciaomondo#;R   G;grgrgrgrgr:ROWOLLEHR
+              GR'heywelt#;G        R;:hallowelt;R   G;grgrgrgr#:R
+              G:talofale;G          R,lalolagi#;R   G;grgrgrgr,R
+              G;гейсвіт#;G          R;привітсвітR   G;grgrgrgr,R
+              G;haiduniyaG          R;haiduniya;R   G;grgrgrgr,R
+              G:heywrâld;G          R,#heywrâld;R   G;grgrgrgr'R
+              G:,kiaorate,G        R;aoheiteao#;R   G;grgrgrgr,R
+               G:salamudunia;HELLO;;#heydunia##;R   G;grgrgrgr,R
+                G:;##γειάσουκόσμεγειάσουκόσμε##;R   G;grgrgrgr;R
+                 GR;;###heimaalimaheimaalima###;R   G;grgrgrgr;R
+                  GR;;sveikapasaulesveikapasaul;R   G;grgrgrgr,R
+                     GRHELLOWORLD##GR,hoymundo#;R   G;++++++++;R
+               GRHELLOWORLD        GR;holamundo:R   GHELLOWORLDR
+               G:talofale;HELLOWORL;;lalolagi#;GR
+               GR;alohahonuaalohahonuaaloha#,;GR
+                GR;,chàothếgiớichàothếgiới,;RGR
+                  GR#;;marhabanbialealam;GRGR
+                    GRGR:,#héwereld#,:GRGR
+                          GHELLOWOR
+        */
+
+        #include <stdio.h>
+        int main(int argc, char *argv[]) {
+            printf("Hello World!");
+            const char *preview = ""
+            return 0;
+        }
+        
+        
+        
+        
+       
+       
